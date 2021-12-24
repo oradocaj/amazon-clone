6238962a9bd93b9dfc1cd325d09c442fc12d36d8
oradocaj <oradocaj@gmail.com>
2021-12-25 00:45:01 +0100

    Added Header, Banner, ProductFeed and Product component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,18 +1,31 @@
 import Head from 'next/head';
+import Banner from '../components/Banner';
 import Header from '../components/Header';
+import ProductFeed from '../components/ProductFeed';
 
-export default function Home() {
+export default function Home({ products }) { // with destructuring we don't have to use Home(props)
   return (
-    <div>
+    <div className='bg-gray-100'>
       <Head>
         <title>Amazon 2.0</title>
       </Head>
-
-      <h1>Hey PAPAFAM</h1>
-
       <Header />
-
-      
+      <main className='max-w-screen-2xl mx-auto'>
+        <Banner />
+        <ProductFeed products={products} />
+      </main>
     </div>
   );
+}
+
+export async function getServerSideProps(context) {
+  const products = await fetch("https://fakestoreapi.com/products").then(
+    (res) => res.json()
+  )
+
+  return {
+    props: {
+      products, //es6 allows us to shorten products: products
+    },
+  };
 }
