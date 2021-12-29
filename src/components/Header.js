fed0272913670d8cfbfe431d85a08e81c4827985
oradocaj <oradocaj@gmail.com>
2021-12-29 21:34:51 +0100

    Started creating checkout page and hooked it with router

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,8 +5,11 @@ import {
     ShoppingCartIcon,
 } from '@heroicons/react/outline';
 import { signIn, signOut, useSession } from "next-auth/react";
+import { useRouter } from 'next/router';
 
 function Header() {
+    const { data: session, status } = useSession();
+    const router = useRouter();
     return (
         <header>
             {/* Top nav */}
@@ -14,6 +17,7 @@ function Header() {
                 <div className="mt-2 flex items-center flex-grow sm:flex-grow-0">
                     {/*Next.js image tag, it optimises image*/}
                     <Image
+                        onClick = {() => router.push('/')} 
                         src='https://links.papareact.com/f90'
                         width={150}
                         height={40}
@@ -30,8 +34,10 @@ function Header() {
                 
                 {/* Right */}
                 <div className="text-white flex items-center text-xs space-x-6 mx-6 whitespace-nowrap">
-                    <div onClick={signIn} className="link">
-                        <p>Hello Ogic Rogic</p>                        
+                    <div onClick={!session ? signIn : signOut} className="cursor-pointer link">
+                        <p>
+                            {session ? `Hello, ${session.user.name}` : "Sign In"}
+                        </p>                        
                         <p className="font-extrabold md:text-sm">Account & Lists</p>
                     </div>
 
@@ -40,10 +46,12 @@ function Header() {
                         <p className="font-extrabold md:text-sm">& Orders</p>
                     </div>
 
-                    <div className="relative link flex items-center">
+                    <div onClick={() => { router.push('/checkout') }} className="relative link flex items-center">
                         <span className="absolute top-0 right-0 md:right-10 h-4 w-4 bg-yellow-400 text-center rounded-full text-black font-bold">0</span>
                         <ShoppingCartIcon className="h-10"/>
-                        <p className="hidden md:inline font-extrabold md:text-sm mt-2">Basket</p>
+                        <p className="hidden md:inline font-extrabold md:text-sm mt-2">
+                            Basket
+                        </p>
                     </div>
                 </div>
             </div>
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,17 @@
+import { Provider } from 'react-redux';
+import { store } from '../app/store';
+//import { Provider as AuthProvider } from 'next-auth/client';
 import '../styles/globals.css'
+import { SessionProvider } from "next-auth/react"
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
+const MyApp = ({ Component, pageProps: {session, ...pageProps} }) => {
+  return (
+    <SessionProvider session={pageProps, session}>
+      <Provider store={store}>
+        <Component {...pageProps} />
+      </Provider>      
+    </SessionProvider>
+  );
+};
 
-export default MyApp
+export default MyApp;
