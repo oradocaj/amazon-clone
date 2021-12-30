489eb27069716990162731592cfd6cf91bbe3bf8
oradocaj <oradocaj@gmail.com>
2021-12-30 14:26:49 +0100

    Built Checkout page with redux and next auth

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,13 +3,16 @@ import {
     MenuIcon,
     SearchIcon,
     ShoppingCartIcon,
-} from '@heroicons/react/outline';
+} from "@heroicons/react/outline";
 import { signIn, signOut, useSession } from "next-auth/react";
-import { useRouter } from 'next/router';
+import { useRouter } from "next/router";
+import { useSelector } from "react-redux";
+import { selectItems } from "../slices/basketSlice";
 
 function Header() {
     const { data: session, status } = useSession();
     const router = useRouter();
+    const items = useSelector(selectItems)
     return (
         <header>
             {/* Top nav */}
@@ -47,7 +50,7 @@ function Header() {
                     </div>
 
                     <div onClick={() => { router.push('/checkout') }} className="relative link flex items-center">
-                        <span className="absolute top-0 right-0 md:right-10 h-4 w-4 bg-yellow-400 text-center rounded-full text-black font-bold">0</span>
+                        <span className="absolute top-0 right-0 md:right-10 h-4 w-4 bg-yellow-400 text-center rounded-full text-black font-bold">{items.length}</span>
                         <ShoppingCartIcon className="h-10"/>
                         <p className="hidden md:inline font-extrabold md:text-sm mt-2">
                             Basket
