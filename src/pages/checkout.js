e6fca8f9c736bbc9d82ba6eee935daad3740f23b
oradocaj <oradocaj@gmail.com>
2021-12-31 19:27:03 +0100

    started making stripe payment session

--- src/pages/checkout.js
+++ src/pages/checkout.js
@@ -5,11 +5,29 @@ import { selectItems, selectTotal } from "../slices/basketSlice";
 import CheckoutProduct from "../components/CheckoutProduct";
 import Currency from "react-currency-formatter";
 import { useSession } from "next-auth/react";
+import { loadStripe } from "@stripe/stripe-js";
+import axios from "axios";
+const stripePromise = loadStripe(process.env.stripe_public_key);
 
 function Checkout() {
     const items = useSelector(selectItems);
     const total = useSelector(selectTotal);
     const { data: session, status } = useSession();
+
+    const createCheckoutSession = async () => {
+        const stripe = await stripePromise;
+
+        // Call the backend to create a checkout session...
+        const checkoutSession = await axios.post("./api/create-checkout-session", {
+            items: items,
+            email: session.user.email,
+        }).then(function (response) {
+            console.log(response);
+          }).catch(function (error) {
+            console.log(error);
+          });
+    };
+
     return (
         <div className="bg-gray-100">
             <Header />
@@ -56,6 +74,8 @@ function Checkout() {
                             </h2>
 
                             <button
+                                role="link"
+                                onClick={createCheckoutSession}
                                 disabled={!session}
                                 className={`button mt-2 ${
                                     !session && 
